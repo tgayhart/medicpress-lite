7b9baf5458693a28b28c42800cf70386989a1f69
Gregor Capuder <capuderg@gmail.com>
2017-03-28 13:47:26 +0200

    Add CI grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -270,4 +270,36 @@ module.exports = function ( grunt ) {
 		'scsslint',
 		'jshint',
 	] );
+
+	// CI
+	// build assets
+	grunt.registerTask( 'ci', 'Builds all assets on the CI, needs to be called with --theme_version arg.', function () {
+		// get theme version, provided from cli
+		var version = grunt.option( 'theme_version' ) || null;
+
+		// check if version is string and is in semver.org format (at least a start)
+		if ( 'string' === typeof version && /^v\d{1,2}\.\d{1,2}\.\d{1,2}/.test( version ) ) { // regex that version starts like v1.2.3
+			var longVersion = version.substring( 1 ).trim(),
+				tasksToRun = [
+					'build',
+					'replace:theme_version',
+					'check_theme_replace',
+					'addtextdomain'
+				];
+
+			grunt.option( 'longVersion', longVersion );
+
+			grunt.task.run( tasksToRun );
+		}
+		else {
+			grunt.fail.warn( 'Version to be replaced in style.css is not specified or valid.\nUse: grunt <your-task> --theme_version=v1.2.3\n', 3 );
+		}
+	} );
+
+	// check if the search-replace was performed
+	grunt.registerTask( 'check_theme_replace', 'Check if the search-replace for theme version was performed.', function () {
+		if ( true !== grunt.option( 'version_replaced_flag' ) ) {
+			grunt.fail.warn( 'Search-replace task error - no theme version replaced.' );
+		}
+	} );
 };
